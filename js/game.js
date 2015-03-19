6d0ba486a893670c8325567fd550f76c0488ba48
Seane235 <Seane235@gmail.com>
2015-03-19 10:48:55 -0400

    click listener attached to all star sprites

--- js/game.js
+++ js/game.js
@@ -9,21 +9,36 @@ function preload(){
 }
 
 function create(){
+	game.stage.backgroundColor = '#313131';
+
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	var stars = game.add.group();
+	stars = game.add.group();
 	stars.enableBody = true;
 
-	for(var i = 0; i < 50; i++){
+	for(var i = 0; i < 12; i++){
 		var star = stars.create(game.world.randomX, game.world.randomY, 'star');
 		star.body.collideWorldBounds = true;
-		star.body.bounce.setTo(0.2, 0.2);
+		star.body.bounce.setTo(0.8, 0.8);
+		star.body.velocity.setTo(getRandomInt(-100,100), getRandomInt(-100,100))
+		star.inputEnabled = true;
+		star.events.onInputDown.add(onDown,this);
 	}
+
 	
-	game.add.sprite(0, 0, 'star');
+
 
 }
 
 function update(){
+	game.physics.arcade.collide(stars);
+}
 
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function onDown(){
+	console.log( 'click' );
 }
